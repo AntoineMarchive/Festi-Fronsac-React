8fe5b889e7cbb6644ef56d5035acc705f9846a4c
AntoineMarchive <antoine.marchive@orange.fr>
2024-10-09 22:08:35 +0200

    Header

---
+++ my-app/src/Components/Header/index.jsx
@@ -0,0 +1,35 @@
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+
+
+export function Header() {
+  return (
+    <header className="header">
+      <figure className="header__fig">
+        <img src={logo} alt="Logo Festi' Fronsac" />
+      </figure>
+      <div className="header__nav">
+      <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+              
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/a-propos"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              A propos
+            </NavLink>
+          </li>
+          </ul>
+      </div>
+    </header>
+  );
+}
