e2c15623b0da9ffb12403618db3850b6b3b74081
AntoineMarchive <antoine.marchive@orange.fr>
2024-10-09 22:23:49 +0200

    footer

--- my-app/src/Components/Footer/index.jsx
+++ my-app/src/Components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import "./Footer.scss";
+import { image } from '../../assest/Images/festi_fronsac_logo.png'
 
-export function Footer() {
+export function Footer(image) {
   return (
     <footer className="footer">
-      <img className="footer_logo" src={logo} alt="Logo Festi' Fronsac"></img>
+      <img className="footer_logo" src={image} alt="Logo Festi' Fronsac"></img>
       <p className="footer__copyrightText">© 2024 Festi' Fronsac. All rights reserved</p>
     </footer>
   );
